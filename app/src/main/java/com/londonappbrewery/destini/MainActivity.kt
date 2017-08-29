8fdc538377baf2a96cf6860584454364dc99fa18
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2017-08-30 07:49:54 +0900

    convert java to kotlin

---
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.londonappbrewery.destini
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // TODO: Steps 4 & 8 - Declare member variables here:
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+
+
+    }
+}
